0ebde87b6573b05ca52378f4129b83e363595941
Frederick Javalera <frederickjavalera@gmail.com>
2021-09-29 08:20:34 -0600

    Initial Commit

---
+++ MatchedGeometryEffect Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchedGeometryEffect Demo
+//
+//  Created by Frederick Javalera on 9/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchedGeometryEffect Demo/MatchedGeometryEffect_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffect_DemoApp.swift
+//  MatchedGeometryEffect Demo
+//
+//  Created by Frederick Javalera on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffect_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
